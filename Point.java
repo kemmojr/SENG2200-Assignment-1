2df5f9547098cb589c72a6578970e3d28095ea1c
Timothy Kemmis <kemmojr@gmail.com>
2020-03-03 15:51:39 +1100

    see above message

--- Point.java
+++ Point.java
@@ -1,8 +1,22 @@
 import java.util.*;
-//import java.
+import java.lang.Math;
 public class Point {
     double x = 1;
     double y = 2;
+
+    public double distance(){
+        double distance = Math.sqrt(x*x+y*y);
+
+        return distance;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
 
     @Override
     public String toString() {
--- Polygon.java
+++ Polygon.java
@@ -1,12 +1,37 @@
 import java.util.*;
 public class Polygon {
 
+    private Point[] points;
     int numOfPoints = 5;
-    private Point[] points = new Point[numOfPoints];
+
+
+    Polygon(int numOfPoints){
+        points = new Point[numOfPoints];
+    }
+
+    public void addPoint(Point p){
+        boolean b = true;
+        int counter = 0;
+        while (b){
+            if (points[counter].getX()!=null){
+
+            }
+        }
+    }
 
     public double distance(){
         double dist = 0;
         return dist;
+    }
+
+    public double area(){
+        double area = 0;
+
+        for (int i = 0; i<numOfPoints-2;i++){
+            area = 1/2*((points[i+1].getX()+points[i].getX())*(points[i+1].getY()-points[i].getY()));
+        }
+        return area;
+
     }
 
     @Override
