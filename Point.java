a0820a92025473f9e61a2eaba118a6707eab8645
Timothy Kemmis <kemmojr@gmail.com>
2020-03-03 14:53:27 +1100

    added Point class

---
+++ Point.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//import java.
+public class Point {
+    double x = 1;
+    double y = 2;
+
+    @Override
+    public String toString() {
+        String out = "(" + x + ", " + y + ")";
+        return out;
+    }
+}
