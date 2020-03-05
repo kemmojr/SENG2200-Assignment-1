bf48a0b30fc5a5c61519b7527a2f1967a0f1ea89
Timothy Kemmis <kemmojr@gmail.com>
2020-03-05 12:33:48 +1100

    Added things to Node class including next, previous and Polygon data and the nessecary getters and setters

--- Node.java
+++ Node.java
@@ -3,4 +3,39 @@ import java.util.*;
 public class Node {
     //next & previous pointers of data type polygon and pointer
     //data = new Polygon
+    private Polygon data;
+    private Node next;
+    private Node previous;
+
+    public Node(Polygon p){
+        next = this;
+        previous = this;
+        data = p;
+    }
+
+    public Node(Polygon p, Node nxt, Node prev){
+        next = nxt;
+        previous = prev;
+        data = p;
+    }
+
+    public void setNext(Node nxt){
+        next = nxt;
+    }
+
+    public void setPrevious(Node prev){
+        next = prev;
+    }
+
+    public void setData(Polygon p){
+        data = p;
+    }
+
+    public Node getNext(){
+        return next;
+    }
+
+    public Node getPrevious(){
+        return previous;
+    }
 }
