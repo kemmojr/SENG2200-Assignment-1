ac6242f23ef4f1663c1a32c363a8c3ac18d648ac
Timothy Kemmis <kemmojr@gmail.com>
2020-03-05 12:52:03 +1100

    Added sentinel and current pointers and constructors to MyPolygons

--- MyPolygons.java
+++ MyPolygons.java
@@ -1,4 +1,18 @@
 import java.util.*;
 public class MyPolygons {
     //Store a doubly linked circular Linked List
+    Node sentinel;
+    Node current;
+
+    public MyPolygons(Polygon first){
+        Node n = new Node(first);
+        sentinel = n;
+        current = n;
+    }
+
+    public MyPolygons(){
+        sentinel = null;
+        current = null;
+        //Node n = new Node();
+    }
 }
