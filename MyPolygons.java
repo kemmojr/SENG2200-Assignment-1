9b0af436f6200e921fc7b80898aa8366a56247d3
Tim Kemmis <kemmojr@gmail.com>
2020-03-21 13:10:45 +1100

    Commented a bunch of code and re-wrote the code in MyPolygons to function without using tail

--- MyPolygons.java
+++ MyPolygons.java
@@ -8,7 +8,7 @@ public class MyPolygons {
     private Node tail;
     private int size = 0;
 
-    public MyPolygons(Polygon first){
+    public MyPolygons(Polygon first){//Creates a MyPolygons object with one node from a polygon
         Node n = new Node(first);
         sentinel = n;
         current = n;
@@ -16,14 +16,14 @@ public class MyPolygons {
         size++;
     }
 
-    public MyPolygons(Node n){
+    public MyPolygons(Node n){//Creates a MyPolygons object with one node from a node
         sentinel = n;
         current = n;
         tail = n;
         size++;
     }
 
-    public MyPolygons(MyPolygons mp){
+    public MyPolygons(MyPolygons mp){//Creates a MyPolygons object with one node from a MyPolygons taking the first node of MyPolygons
         mp.current = mp.sentinel;
         sentinel = mp.current;
         current = mp.current;
@@ -31,13 +31,13 @@ public class MyPolygons {
         size++;
     }
 
-    public MyPolygons(){
+    public MyPolygons(){//Empty constructor
         sentinel = null;
         current = null;
         //Node n = new Node();
     }
 
-    public void insert(Polygon p){
+    public void insert(Polygon p){//Insert a polygon in a node before current
         Node n = new Node(p);
         n.setNext(current);
         n.setPrevious(current.getPrevious());
@@ -50,22 +50,26 @@ public class MyPolygons {
 
     }
 
-    public void prepend(Polygon p){
+    public void prepend(Polygon p){//Add a new node at the head of the LL
         Node n = new Node(p);
         n.setNext(sentinel);
         sentinel.setPrevious(n);
-        tail.setNext(n);
-        n.setPrevious(tail);
+        //tail.setNext(n);
+        sentinel.getPrevious().setNext(n);
+        //n.setPrevious(tail);
+        n.setPrevious(sentinel.getPrevious());
         n.setNext(sentinel);
         sentinel = n;
         size++;
     }
 
-    public void append(Polygon p){
+    public void append(Polygon p){//Add a new node at the end of the LL
         Node n = new Node(p);
-        n.setPrevious(tail);
+        //n.setPrevious(tail);
+        n.setPrevious(sentinel.getPrevious());
         n.setNext(sentinel);
-        tail.setNext(n);
+        //tail.setNext(n);
+        sentinel.getPrevious().setNext(n);
         sentinel.setPrevious(n);
         tail = n;
         size++;
@@ -73,14 +77,16 @@ public class MyPolygons {
 
     public void remove(){//remove from the head of the list
         Node newSentinel = sentinel.getNext();
-        newSentinel.setPrevious(tail);
-        tail.setNext(newSentinel);
+        //newSentinel.setPrevious(tail);
+        newSentinel.setPrevious(sentinel.getPrevious());
+        //tail.setNext(newSentinel);
+        sentinel.getPrevious().setNext(newSentinel);
         sentinel.delete();
         sentinel = newSentinel;
         size--;
     }
 
-    public void reset(){
+    public void reset(){//Set current to the sentinel
         current = sentinel;
     }
 
@@ -98,9 +104,9 @@ public class MyPolygons {
         current = current.getPrevious();
     }
 
-    public Node getCurrent(){
+    /*public Node getCurrent(){
         return current;
-    }
+    }*/
 
     public void insertSorted(MyPolygons mp){//Inserts a node into its correct position with the node being the current of mp to avoid breaking encapsulation
         reset();
@@ -144,7 +150,7 @@ public class MyPolygons {
     }
 
     @Override
-    public String toString() {
+    public String toString() {//A toString method that steps through the LinkedList and outputs in in the correct format
         String out = "";
         current = sentinel;
         for (int i=0;i<size;i++){
